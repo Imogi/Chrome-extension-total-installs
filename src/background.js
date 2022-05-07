fb933d7695f99197e8d85b833aca2942f415b01a
rickygian <rickygiankwan@gmail.com>
2022-05-07 17:41:58 +1000

    Created basic logo, directory structure, and basic landing page

---
+++ src/background.js
@@ -0,0 +1,15 @@
+// Shows the default html page once the extension is installed
+// I.e in the future this can just direct us to a website, for 
+// now it isn't useful
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.tabs.create({
+//     url: ''
+//   })
+// })
+
+
+// function openApp() {
+//   chrome.app.window.create({url: './popups/default-popup.html'})
+// }
+
+// chrome.action.onClicked.addListener(openApp);
